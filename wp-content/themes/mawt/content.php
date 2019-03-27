841f2a917d85b9836bf97a01427474f81fe7e30c
Backtun <backunn@gmail.com>
2019-03-26 18:05:23 -0600

    Se agreg the loop y metodos de wordpress

--- wp-content/themes/mawt/content.php
+++ wp-content/themes/mawt/content.php
@@ -1,6 +1,24 @@
 <article class="Content">
-    <h2>Contendio</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus quas itaque, assumenda deleniti
-        dolorem libero vero rerum accusantium saepe pariatur sunt, corporis cumque omnis quam enim expedita,
-        architecto quisquam voluptatum!</p>
+    <h1>Creación de temas</h1>
+    <?php if(have_posts()):while(have_posts()): the_post(); ?>
+    <article>
+        <h2><?php the_title(); ?></h2>
+        <?php the_title('<h3 class="TitlePost">', '</h3>') ?>
+        <?php 
+        $titulo = 'Titulo: '. get_the_title();
+        echo $titulo;
+         ?>
+        <?php permalink_link(); ?>
+        <h2>
+            <a href="<?php permalink_link(); ?>"><?php the_title(); ?></a>
+        </h2>
+        <?php the_excerpt(); ?>
+        <?php the_category(); ?>
+        <p><?php the_category(', '); ?></p>
+        <p><?php the_category(', '); ?></p>
+        <hr>
+    </article>
+    <?php endwhile; else: ?>
+    <p>El contenido solicitado no existe</p>
+    <?php endif; ?>
 </article>
